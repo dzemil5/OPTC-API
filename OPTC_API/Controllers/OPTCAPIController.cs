6cc6ff23a19b1387906ab953390bfe63d84e8c4a
dzemil5 <dzemil.sahovic@gmail.com>
2023-01-25 20:20:57 +0100

    Revert "A very simple One Piece Treasure Cruise Character API with a single model. RESTful API with .NET Core practice."
    This reverts commit c37c333b79772c10914fade6aaf6e495453ec288.

--- OPTC_API/Controllers/OPTCAPIController.cs
+++ OPTC_API/Controllers/OPTCAPIController.cs
@@ -1,181 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 using OPTC_API.Models;
-using OPTC_API.Data;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.JsonPatch;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 
 namespace OPTC_API.Controllers
 {
-    [Route("api/OPTCAPI")]
+    [Route("api/[controller]")]
     [ApiController]
     public class OPTCAPIController : ControllerBase
     {
-        private readonly ILogger<OPTCAPIController> _logger;
-        private readonly AppDbContext _db;
-
-        public OPTCAPIController(AppDbContext db, ILogger<OPTCAPIController> logger)
-        {
-            _db = db;
-            _logger = logger;
-        } 
-
         [HttpGet]
-        public ActionResult<IEnumerable<Character>> GetCharacters()
+        public IEnumerable<Character> GetCharacters()
         {
-            _logger.LogInformation("Getting all characters");
-            return Ok(_db.Characters);
-        }
+            
+            return new List<Character> {
+            new Character{Id=0002, Name= "Monkey D. Luffy", Type="STR", Class1="Fighter", Class2=null, Cost=4, Stars=3},
+            new Character{Id=0005, Name= "Roronoa Zoro", Type="DEX", Class1="Slasher", Class2=null, Cost=4, Stars=3},
+            new Character{Id=0009, Name= "Nami", Type="INT", Class1="Striker", Class2=null, Cost=4, Stars=3},
+            new Character{Id=0013, Name= "Usopp", Type="PSY", Class1="Shooter", Class2=null, Cost=4, Stars=3},
+            new Character{Id=0017, Name= "Sanji", Type="QCK", Class1="Fighter", Class2=null, Cost=4, Stars=3},
+            new Character{Id=0021, Name= "Tony Tony Chopper", Type="PSY", Class1="Fighter", Class2=null, Cost=4, Stars=3}
 
-
-
-        [HttpGet("{id:int}", Name = "GetCharacter")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
-        public ActionResult<Character> GetCharacter(int id)
-        {
-            if (id == 0)
-            {
-                _logger.LogError("Failed to get Character With ID:" + id);
-                return BadRequest();
-            }
-            var character = _db.Characters.FirstOrDefault(u => u.Id == id);
-
-            if (character == null)
-            {
-                return NotFound();
-            }
-            return Ok(character);
-        }
-
-
-
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
-        public ActionResult<Character> AddCharacter([FromBody] Character character)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(character);
-            }
-            if (_db.Characters.FirstOrDefault(u => u.Id == character.Id) != null)
-            {
-                ModelState.AddModelError("", "Character already exists");
-                return BadRequest(ModelState);
-            }
-            if (character == null)
-            {
-                return BadRequest(character);
-            }
-            _db.Characters.AddAsync(character);
-
-            Character model = new()
-            {
-                Id = character.Id,
-                Name = character.Name,
-                Type = character.Type,
-                Class1 = character.Class1,
-                Class2 = character.Class2,
-                Cost = character.Cost,
-                Stars = character.Stars,
-                Image = character.Image
             };
-            //_db.Characters.Add(model);
-            _db.SaveChanges();
-
-
-
-            return CreatedAtRoute("GetCharacter", new { id = character.Id }, character);
         }
-        [HttpDelete("{id:int}", Name = "DeleteCharacter")]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult DeleteCharacter(int id)
-        {
-            if (id == 0)
-            {
-                return BadRequest();
-            }
-            var character = _db.Characters.FirstOrDefault(u => u.Id == id);
-            if (character == null)
-            {
-                return NotFound();
-            }
-            _db.Characters.Remove(character);
-            _db.SaveChanges();
-            return NoContent();
-        }
-        [HttpPut("{id:int}", Name = "UpdateCharacter")]
-        public IActionResult UpdateCharacter(int id, [FromBody] Character character)
-        {
-            if (character == null || id != character.Id)
-            {
-                return BadRequest();
-            }
-            Character model = new()
-            {
-                Id = character.Id,
-                Name = character.Name,
-                Type = character.Type,
-                Class1 = character.Class1,
-                Class2 = character.Class2,
-                Cost = character.Cost,
-                Stars = character.Stars,
-                Image = character.Image
-            };
-            _db.Characters.Update(model);
-            _db.SaveChanges();
-
-            return NoContent();
-        }
-        [HttpPatch("{id:int}", Name = "UpdatePartialCharacter")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult UpdatePartialCharacter(int id, JsonPatchDocument<Character> patchChar)
-        {
-            if (patchChar == null || id == 0)
-            {
-                return BadRequest();
-            }
-            var charac = _db.Characters.AsNoTracking().FirstOrDefault(u => u.Id == id);
-            Character character = new()
-            {
-                Id = charac.Id,
-                Name = charac.Name,
-                Type = charac.Type,
-                Class1 = charac.Class1,
-                Class2 = charac.Class2,
-                Cost = charac.Cost,
-                Stars = charac.Stars,
-                Image = charac.Image
-            };
-            patchChar.ApplyTo(character, ModelState);
-            Character model = new Character()
-            {
-                Id = character.Id,
-                Name = character.Name,
-                Type = character.Type,
-                Class1 = character.Class1,
-                Class2 = character.Class2,
-                Cost = character.Cost,
-                Stars = character.Stars,
-                Image = character.Image
-            };
-            _db.Characters.Update(model);
-            _db.SaveChanges();
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            return NoContent();
-        }
+        
     }
 }
--- OPTC_API/Program.cs
+++ OPTC_API/Program.cs
@@ -1,23 +1,8 @@
-using Serilog;
-using System.Diagnostics;
-using Microsoft.EntityFrameworkCore;
-using OPTC_API.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("./Logs/optclog.txt", rollingInterval: RollingInterval.Hour).CreateLogger();
-
-builder.Host.UseSerilog();
-
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
-});
-builder.Services.AddControllers(option =>
-{
-    option.ReturnHttpNotAcceptable=true;
-}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
